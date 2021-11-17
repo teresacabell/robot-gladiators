820fe7d195a308a951596acb2fd8c3836d12136d
teresacabell <teresacabll@gmail.com>
2021-11-17 10:58:21 -0600

    added concatenated strings

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,18 @@
 
 var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName)
+
+console.log("this logs a string, good for leaving yourself a message")
+ // this will do math and log 20
+ console.log(10 + 10)
+ // what is this?
+ console.log("Our robot's name is " + playerName);
+var name = "your name";
+console.log(name);
 
 // this creates a function named "fight"
-
     function fight() {
         window.alert("The fight has begun!");
     }
-
-    fight();
+   // fight();
